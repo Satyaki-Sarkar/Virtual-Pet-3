22484fc90dcc8eedb6edc2b5d5f25c06769221c7
Satyaki <satyakisarkar7500@gmail.com>
2020-09-18 21:23:07 +0530

    Virtual Pet 1 Completed Activity C34

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 //Create variables here
 var dog, happyDog, dogImage, database, foodS, foodStock, DogImage, HappyDogImage;
 var dogFoodCounter,c,foodRemaining;
+var gameState="PLAY";
 
 function preload()
 {
@@ -30,29 +31,50 @@ function setup()
 function draw() 
 {  
   background(46,139,87);
+  if(gameState==="PLAY")
+  {
 
-  if(dogFoodCounter==="Food Remaining"){
-    if(keyWentDown(UP_ARROW))
-    {
-      writeStock(foodS);
-      dog.addImage(happyDog);
-      c=20;
-      foodS--;
+    if(dogFoodCounter==="Food Remaining"){
+      if(keyWentDown(UP_ARROW))
+      {
+        writeStock(foodS);
+        dog.addImage(happyDog);
+        c=20;
+        foodS--;
+      }
+    }else {
+      gameState="END";
+    }
+
+    c--;
+    if(c<=0){
+      dog.addImage(dogImage);
+    }
+
+  }else  if(gameState==="END")
+  {
+    dog.addImage(dogImage);
+    if(keyCode===32){
+      gameState="PLAY";
+      foodS=20;
+      dogFoodCounter="Food Remaining";
+      foodStock.update({
+        Food : 20
+      });
     }
   }
-
-  c--;
-  if(c<=0){
-    dog.addImage(dogImage);
-  }
   drawSprites();
-  //add styles here
-  textSize(20);
-  fill("Black");
-  if(dogFoodCounter==="Food Remaining"){
-    text("Food Remaining : "+foodRemaining,20,20);
-    text("NOTE : Press UP_ARROW to feed Rocky milk.",20,480);
-  }else {
+    //add styles here
+    textSize(20);
+    fill("Black");
+    if(dogFoodCounter==="Food Remaining"){
+      text("Food Remaining : "+foodRemaining,20,20);
+      text("NOTE : Press UP_ARROW to feed Rocky milk.",20,480);
+    }
+
+    if(dogFoodCounter==="Food Over")
+  {
+    text("Press SPACE to reset.",20,480)
     text("Food Over",20,20);
   }
 
@@ -63,9 +85,11 @@ function writeStock(x){
  if(x<=1){
     x=0;
     dogFoodCounter="Food Over";
+    gameState="END";
   } else {
     x--;
   }
+  console.log(x);
 
   foodStock.update({
     Food : x 
